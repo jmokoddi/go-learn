0f23db065508ff4236c1b868240983f9245a2dcd
Justin Onstot <justin.onstot@koddi.com>
2021-10-20 19:08:32 -0700

    commit 1

---
+++ testinggit/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// one way - declare then assign (two steps)
+	firstNumber := 2
+
+	fmt.Println("This thing is neat")
+	fmt.Println(firstNumber)
+
+}
