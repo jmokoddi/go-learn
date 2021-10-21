e79c5c0f4ad92496f6c77bf7d11a75130242ab69
Justin Onstot <justin.onstot@koddi.com>
2021-10-20 19:16:09 -0700

    commit 2

--- testinggit/main.go
+++ testinggit/main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
 	// one way - declare then assign (two steps)
 	firstNumber := 2
 
-	fmt.Println("This thing is neat")
-	fmt.Println(firstNumber)
-
+	fmt.Println("This thing is neat " + strconv.Itoa(firstNumber))
 }
